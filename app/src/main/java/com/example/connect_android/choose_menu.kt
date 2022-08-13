b644282e31bacbef8d52416f359d87ba7a7ca86a
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-13 20:28:30 +0900

    layout생성, listView 커스텀 제작

---
+++ app/src/main/java/com/example/connect_android/choose_menu.kt
@@ -0,0 +1,11 @@
+package com.example.connect_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class choose_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_menu)
+    }
+}
