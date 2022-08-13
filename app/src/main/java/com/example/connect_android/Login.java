901264f3e9a7877da874b3e3059d0c008dc9fad8
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-14 03:51:10 +0900

    버튼업데이트

--- app/src/main/java/com/example/connect_android/Login.java
+++ app/src/main/java/com/example/connect_android/Login.java
@@ -4,16 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Login extends AppCompatActivity {
-
+    EditText email_text;
+    EditText password_text;
+    Button login_btn;
     Button siqnup_btn;
+
+    private FirebaseAuth mAuth;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email_text = findViewById(R.id.email_text);
+        password_text = findViewById(R.id.password_text);
+        login_btn = findViewById(R.id.login_btn);
         siqnup_btn = findViewById(R.id.siqnup_btn);
 
         siqnup_btn.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +33,16 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 Intent in = new Intent(Login.this, SignUp.class);
                 startActivity(in);
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin();
+            }
+
+            private void userLogin() {
             }
         });
     }
