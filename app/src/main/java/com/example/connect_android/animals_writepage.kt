c5d3a582f9e796a5e70e10625c3bde4c60d98f50
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-14 00:57:18 +0900

    반려동물찾기 글 쓰는 페이지 xml 파일 생성

---
+++ app/src/main/java/com/example/connect_android/animals_writepage.kt
@@ -0,0 +1,11 @@
+package com.example.connect_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class animals_writepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animals_writepage)
+    }
+}
