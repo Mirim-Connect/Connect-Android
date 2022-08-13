92c98bcf41983e889b0e56961773c2ec42ee5510
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-08-13 23:10:34 +0900

    글쓰는페이지 xml, kt파일 추가

---
+++ app/src/main/java/com/example/connect_android/Human_writepage.kt
@@ -0,0 +1,11 @@
+package com.example.connect_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Human_writepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_human_writepage)
+    }
+}
